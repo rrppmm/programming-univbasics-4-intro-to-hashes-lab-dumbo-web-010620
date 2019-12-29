9a9d52f7d360d87b56d43d6d0862afde03c3c800
Paul M <paulmcadory@gmail.com>
2019-12-29 22:06:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,19 +3,19 @@ def new_hash
 end
 
 def my_hash
-  hash = { me: "you"};
+  hash = { me: "you" };
 end
 
 def pioneer
-  pioneer = { :name => 'Grace Hopper'};
+  pioneer = { :name => 'Grace Hopper' };
 end
 
 def id_generator
-  number = { :id => 5};
+  number = { :id => 5 };
 end
 
 def my_hash_creator(key, value)
-  another_hash = { key => value};
+  another_hash = { key => value };
 end
 
 def read_from_hash(hash, key)
